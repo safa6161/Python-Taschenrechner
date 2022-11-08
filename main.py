4901066b4d8fc308d40da5c056847c4b9199e1c1
Safa Mazlum <109605597+safa6161@users.noreply.github.com>
2022-11-08 16:42:45 +0300

    Python Taschenrechner

---
+++ main.py
@@ -0,0 +1,31 @@
+print("""*************************
+Taschenrechner Program
+
+Rechenzeichen:
+
+1. Addieren
+
+2. Subtrahieren
+
+3. Multiplizieren
+
+4. Dividieren
+*************************
+""")
+
+a = int (input("1. Zahl:"))
+b = int (input("2. Zahl:"))
+
+Rechenzeichen=input("Rechenzeichen eingeben:")
+
+if Rechenzeichen=="1":
+    print("{} plus {} ergibt gleich {}".format(a,b,a+b))
+elif Rechenzeichen == "2":
+        print("{} minus {} ergibt gleich {}".format(a, b, a - b))
+elif Rechenzeichen=="3":
+    print("{} mal {} ergibt gleich {}".format(a,b,a * b))
+elif Rechenzeichen=="4":
+    print("{} geteilt durch {} ergibt gleich {}".format(a,b,a / b))
+else:
+    print("Eingabe Ungültig")
+    
